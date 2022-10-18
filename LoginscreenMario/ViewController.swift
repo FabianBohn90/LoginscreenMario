0678cc42a24c0807f46ac9da45044ccc2e80a33b
fabianbohn <Mystek.fb@googlemail.com>
2022-10-18 13:37:31 +0200

    Zusatz aufgabe Fertig

--- LoginscreenMario/ViewController.swift
+++ LoginscreenMario/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameInputtxt: UITextField!
+    
+    @IBOutlet weak var passwortInputtxt: UITextField!
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        if usernameInputtxt.text == "Mario"  {
+            usernameInputtxt.text = "Hallo Mario"
+            
+        }else{
+            usernameInputtxt.text = "Falscher Username"
+        }
+        
+        
+        if passwortInputtxt.text == "Super64" {
+            passwortInputtxt.text = "Du bist eingeloggt"
+            
+        }else {
+            passwortInputtxt.text = "Das Passwort ist falsch"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
